1acf21b528959db04949cc87a829658671ec9d4d
Luca Somaschini <somas99@gmail.com>
2022-09-12 16:41:34 +0200

    Snack_4: completed

---
+++ php_snack_4/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Snack_4</title>
+</head>
+<body>
+
+    <?php
+        // creo array inizialmente vuoto
+        $myArray = [];
+
+        // attraverso un ciclo creo 15 numeri casuali
+        for ($i = 0; count($myArray) < 15; $i++) {
+            $newInteger = rand(1, 100);
+            if (!in_array($newInteger, $myArray)) {
+                $myArray[] = $newInteger;
+            }
+        }
+
+        // debug
+        var_dump($myArray);
+    ?>
+
+</body>
+</html>
---
+++ php_snack_4/readme.md
@@ -0,0 +1,3 @@
+## Snack 4
+
+Creare un array con 15 numeri casuali, tenendo conto che l’array non dovrà contenere lo stesso numero più di una volta.
