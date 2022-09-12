3a1c33820a70cbc4170d06b1d48e2b4e04a95698
Luca Somaschini <somas99@gmail.com>
2022-09-12 16:30:34 +0200

    Snack_2: improved comments

--- php_snack_2/index.php
+++ php_snack_2/index.php
@@ -4,17 +4,19 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Snack_5</title>
+    <title>Snack_2</title>
 </head>
 <body>
 
     <?php
-        // passo come parametro get i tre dati
+        // url da usare come test: php_snack_2/?name=giovanni&email=ciao@google.com&age=99
+
+        // passo come parametro get i tre dati.
         $myName = $_GET['name'];
         $myEmail = $_GET['email'];
         $myAge = $_GET['age'];
 
-        // verifica
+        // verifico le condizioni come da richiesta.
         if (strlen($myName) > 3 && strpos($myEmail, '@') && strpos($myEmail, '.') && is_numeric($myAge)) {
             echo 'Accesso riuscito.';
         } else {
